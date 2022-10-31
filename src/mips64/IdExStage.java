374ab7ba5668dad29056ff797f3da92ddc46c1a5
yaboiksa <ky.samuel1@gmail.com>
2022-10-30 20:10:24 -0400

    IdExStage - Added some to update

--- src/mips64/IdExStage.java
+++ src/mips64/IdExStage.java
@@ -15,10 +15,27 @@ public class IdExStage {
     }
 
     int getIntRegister(int regNum) {
-        // todo - add supporting code
+        // todo - add supporting code        
         return 0;
     }
 
     public void update() {
+        instPC = simulator.getIfIdStage().instPC;
+        opcode = simulator.getIfIdStage().opcode;
+
+        Instruction decodedInst = Instruction.getInstructionFromOper(opcode);
+
+        if (decodedInst instanceof ITypeInst){
+            regAData = getIntRegister(((ITypeInst)decodedInst).getRT());
+            regBData = getIntRegister(((ITypeInst)decodedInst).getRS());
+            immediate = ((ITypeInst)decodedInst).getImmed();
+        }
+        else if (decodedInst instanceof RTypeInst){
+            regAData = getIntRegister(((RTypeInst)decodedInst).getRT());
+            regBData = getIntRegister(((RTypeInst)decodedInst).getRS());
+        }
+        else if (decodedInst instanceof JTypeInst){
+            immediate = ((JTypeInst)decodedInst).getOffset();
+        }
     }
 }
