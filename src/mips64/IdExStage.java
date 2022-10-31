08c4fd6f8bd91e49210b17d276397b222153fe2e
yaboiksa <ky.samuel1@gmail.com>
2022-10-30 21:32:04 -0400

    Added default case IdExStage

--- src/mips64/IdExStage.java
+++ src/mips64/IdExStage.java
@@ -1,7 +1,5 @@
 package mips64;
 
-import java.io.Console;
-
 public class IdExStage {
 
     PipelineSimulator simulator;
@@ -11,7 +9,8 @@ public class IdExStage {
     int regAData;
     int regBData;
     int immediate;
-    int inst;
+    //int inst;
+    Instruction inst;
 
     int registers[];
 
@@ -19,6 +18,8 @@ public class IdExStage {
         simulator = sim;
         registers = new int[32];
         registers[0] = 0;
+        opcode = Instruction.getOpcodeFromName("NOP");
+        inst = Instruction.getInstructionFromName("NOP");
     }
 
     Integer getIntRegister(int regNum) {
@@ -28,25 +29,26 @@ public class IdExStage {
     public void update() {
         instPC = simulator.getIfIdStage().instPC;
         opcode = simulator.getIfIdStage().opcode;
+        inst = simulator.getIfIdStage().inst;
 
         Instruction decodedInstruction = Instruction.getInstructionFromName(Instruction.getNameFromOpcode(opcode));
 
         if (Instruction.getNameFromOpcode(opcode) == ""){
         }
         else if (decodedInstruction instanceof ITypeInst){
-            regAData = getIntRegister(((ITypeInst)decodedInstruction).getRT());
-            regBData = getIntRegister(((ITypeInst)decodedInstruction).getRS());
-            immediate = ((ITypeInst)decodedInstruction).getImmed();
+            regAData = getIntRegister(((ITypeInst)inst).getRT());
+            regBData = getIntRegister(((ITypeInst)inst).getRS());
+            immediate = ((ITypeInst)inst).getImmed();
         }
         else if (decodedInstruction instanceof RTypeInst){
-            regAData = getIntRegister(((RTypeInst)decodedInstruction).getRT());
-            regBData = getIntRegister(((RTypeInst)decodedInstruction).getRS());
+            regAData = getIntRegister(((RTypeInst)inst).getRT());
+            regBData = getIntRegister(((RTypeInst)inst).getRS());
             //immediate = 0;
         }
         else if (decodedInstruction instanceof JTypeInst){
             //regAData = 0;
             //regBData = 0;
-            immediate = ((JTypeInst)decodedInstruction).getOffset();
+            immediate = ((JTypeInst)inst).getOffset();
         }
     }
 }
